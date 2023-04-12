39aa8ec968e8edb0a4afd189ba9e8961e1e9d8ff
Melwin <100216266+MelwinC@users.noreply.github.com>
2023-04-13 00:06:43 +0200

    fix cols type

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -51,8 +51,8 @@ const ContactForm = () => {
                             className="w-full px-5 py-2 border border-primary-dark border-opacity-50 text-secondary-light bg-ternary-dark rounded-md shadow-sm text-md"
                             id="message"
                             name="message"
-                            cols="14"
-                            rows="6"
+                            cols={14}
+                            rows={6}
                             aria-label="Message"
                         ></textarea>
                     </div>
