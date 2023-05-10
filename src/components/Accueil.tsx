12f2378735d5c0f38e13ffdad443fe3154749b7b
Melwin Chenu <melwinchn@gmail.com>
2023-05-10 11:59:37 +0200

    CV

--- src/components/Accueil.tsx
+++ src/components/Accueil.tsx
@@ -13,7 +13,7 @@ const Accueil = () => {
                         Développeur d'applications métier & web.
                     </p>
                     <div className="flex justify-center sm:block">
-                        <a download="CV Melwin CHENU.pdf" href="/src/assets/docs/CV_Melwin_CHENU.pdf"
+                        <a download="CV Melwin CHENU.pdf" href="/CV-Melwin-CHENU.pdf"
                             className="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500" aria-label="Download Resume">
                             <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"></circle><polyline points="8 12 12 16 16 12"></polyline><line x1="12" y1="8" x2="12" y2="16"></line></svg>
                             <span className="text-sm sm:text-lg duration-100">
