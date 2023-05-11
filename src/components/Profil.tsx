f52699ecce3950cbfad5233a8f6cfd8b32d38fcf
Melwin Chenu <melwinchn@gmail.com>
2023-05-11 09:52:00 +0200

    Parcours pro (timeline)

--- src/components/Profil.tsx
+++ src/components/Profil.tsx
@@ -1,3 +1,5 @@
+import TimelineParcoursPro from "../assets/TimelineParcoursPro.png"
+
 const Profil = () => {
     return (
         <div className="bg-primary-dark font-poppins min-h-screen">
@@ -20,24 +22,9 @@ const Profil = () => {
                         Cette option fût mon entrée dans le monde du <span className="text-indigo-300">développement</span>.<br />
                         J'ai débuté avec l'<span className="text-indigo-300">HTML5</span>, le <span className="text-indigo-300">CSS3</span> et le <span className="text-indigo-300">JS</span>.
                     </p>
-                    <p className="text-sm mt-14 md:text-lg">
-                        Ayant obtenu mon Bac S, j'ai poursuivi en effectuant un service civique où j'avais pour mission d'assister les <span className="text-indigo-300">utilisateurs</span> à l'utilisation des <span className="text-indigo-300">outils numériques</span>, mission effectuée à Pôle Emploi.<br />
-                        Une expérience enrichissante qui m'a appris le monde du travail.
-                    </p>
-                    <p className="text-sm mt-14 md:text-lg">
-                        Étant aussi intéressé par l'environnement <span className="text-indigo-300">système / hardware</span>, j'ai suivi une formation de TAI (Technicien d'Assistance en Informatique).
-                    </p>
-                    <p className="text-sm mt-14 md:text-lg">
-                        Ce qui m'a amené à réfléchir sur la filière dans laquelle je voulais poursuivre, 2 choix s'offraient à moi :
-                        <ul className="list-disc mt-6 ml-24">
-                            <li>BTS SIO <span className="text-indigo-300">SISR</span></li>
-                            <li>BTS SIO <span className="text-indigo-300">SLAM</span></li>
-                        </ul>
-                    </p>
-                    <p className="text-sm mt-14 md:text-lg">
-                        Mon choix s'est porté sur l'option <span className="text-indigo-300">SLAM</span>, j'ai donc saisi l'occasion de rejoindre cette formation au Greta de Vannes.
-                    </p>
+                    <img src={TimelineParcoursPro} alt="Schéma parcours professionnel" className="mt-20 mx-auto w-5/6" />
                 </div>
+
                 <div className="mt-20">
                     <h1 className="text-2xl md:text-3xl xl:text-4xl text-center mt-20">
                         Mes compétences
